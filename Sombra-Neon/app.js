478a30fc842cc455fcef176e54bebb15a37fd08f
CesarRincon <90701896+CesarRincon@users.noreply.github.com>
2022-06-06 13:36:58 -0500

    Botones music personalidados

---
+++ Sombra-Neon/app.js
@@ -0,0 +1,34 @@
+const player = document.getElementById('player')
+console.log(player);
+console.log(player.paused)
+//Funcion para darle play a la music
+function play() {
+    if (player.paused) {
+        toggleIcon();
+        return player.play();
+    } else {
+        toggleIcon();
+        return player.pause();
+    }
+}
+
+function toggleIcon() {
+    const iconPlay = document.getElementById('iconPlay');
+    iconPlay.classList.toggle('fa-pause');
+    iconPlay.classList.toggle('fa-play');
+}
+
+//Funcion para cambiar de icono cuando termine cambie de cancion
+
+function classIconPlay() {
+    const iconPlay = document.getElementById('iconPlay');
+    iconPlay.classList.remove('fa-solid fa-play')
+    iconPlay.classList.add('fa-solid fa-pause')
+}
+
+
+
+
+
+
+
